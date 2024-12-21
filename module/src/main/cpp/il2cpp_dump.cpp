4f7312408894fee1225e93b12036b2e40cc73c35
Slamifoe21 <135305786+Slamifoe21@users.noreply.github.com>
2024-12-21 14:06:44 +0300

    Update il2cpp_dump.cpp

--- module/src/main/cpp/il2cpp_dump.cpp
+++ module/src/main/cpp/il2cpp_dump.cpp
@@ -104,7 +104,7 @@ bool write_mem(pid_t pid, uint64_t address, char* hex, size_t size)
 	{
 		return false;
 	}
-	fseek(fp, address, SEEK_SET);
+	fseeko(fp, address, SEEK_SET);
 	bool ok = fwrite(hex, sizeof(char), size, fp) == size;
 	fclose(fp);
 	return ok;
@@ -124,9 +124,9 @@ std::string dump_method(Il2CppClass *klass) {
 	    
             //enable maphack
             if (strcmp(il2cpp_method_get_name(method), "get_m_CanSight") == 0) {
-		  *reinterpret_cast<uint64_t*>(method->methodPointer) = 0x0100A0E31EFF2FE1;
-		  LOGI("get_m_CanSight not found!");
-            //    write_mem(getpid(), (uint64_t) method->methodPointer, "\x01\x00\xA0\xE3\x1E\xFF\x2F\xE1", 8);
+		  //*reinterpret_cast<uint64_t*>(method->methodPointer) = 0x0100A0E31EFF2FE1;
+		  LOGI("get_m_CanSight found!");
+                  write_mem(getpid(), (uint64_t) method->methodPointer, "\x01\x00\xA0\xE3\x1E\xFF\x2F\xE1", 8);
             }
         } else {
             outPut << "\t// RVA: 0x VA: 0x0";
